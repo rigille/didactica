a70846d034b18882db355f5891463edbd179daf8
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2023-04-15 11:55:18 -0300

    parse input and treat errors

--- guessing_game/bin/main.ml
+++ guessing_game/bin/main.ml
@@ -1,5 +1,7 @@
 let () =
   print_endline "Guess the number!";
   print_endline "Please input your guess.";
-  let guess = read_line () in
-  print_endline ("Your guess is: " ^ guess)
+  let user_input = read_line () in
+  match int_of_string_opt user_input with
+  | Some guess -> print_endline ("Your guess is: " ^ (string_of_int guess))
+  | None -> print_endline "Please guess a number"
