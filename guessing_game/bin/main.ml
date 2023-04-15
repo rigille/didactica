8ad99b2e7057fa96c5c4786d774f26ee2aca455b
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2023-04-15 12:10:33 -0300

    generate secret random number

--- guessing_game/bin/main.ml
+++ guessing_game/bin/main.ml
@@ -1,7 +1,9 @@
 let () =
   print_endline "Guess the number!";
+  let secret_number = Random.int 100 in
+  print_endline ("DEBUG: the number is: " ^ (string_of_int secret_number));
   print_endline "Please input your guess.";
   let user_input = read_line () in
   match int_of_string_opt user_input with
   | Some guess -> print_endline ("Your guess is: " ^ (string_of_int guess))
-  | None -> print_endline "Please guess a number"
+  | None -> print_endline "Please guess a reasonably small number"
