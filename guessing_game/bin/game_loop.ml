abdfafebd4515c150cf0f77ea5e02150c90f6fc5
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2023-04-19 12:05:20 -0300

    improve messages further

--- guessing_game/bin/game_loop.ml
+++ guessing_game/bin/game_loop.ml
@@ -1,6 +1,9 @@
 open Comparison;;
-let rec game_loop (secret_number : int) : unit =
-  print_endline "Please input your guess.";
+let rec game_loop (secret_number : int) (first : bool) : unit =
+  if first then
+    print_endline "What's your guess?"
+  else
+    print_endline "Try again. What's your next guess?";
   print_string "> ";
   let user_input = read_line () in
   match int_of_string_opt user_input with
@@ -8,12 +11,12 @@ let rec game_loop (secret_number : int) : unit =
     (match compare guess secret_number with
     | Less ->
       print_endline "Too small!";
-      game_loop secret_number
+      game_loop secret_number false
     | Greater ->
       print_endline "Too big!";
-      game_loop secret_number
+      game_loop secret_number false
     | Equal ->
       print_endline "You won, congratulations!")
   | None ->
     print_endline "The guess needs to be a reasonably small natural number";
-    game_loop secret_number;;
+    game_loop secret_number false;;
--- guessing_game/bin/guessing_game.ml
+++ guessing_game/bin/guessing_game.ml
@@ -4,6 +4,6 @@ let main () =
   print_endline "Guess the number!";
   Random.self_init ();
   let secret_number = Random.int 100 in
-  game_loop secret_number;;
+  game_loop secret_number true;;
 
 main ()
