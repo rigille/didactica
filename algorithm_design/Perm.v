fb1d6bb3ff0eff99069769d91200b546f9c3c328
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2023-08-06 21:07:51 -0300

    why is this so hard

--- algorithm_design/Perm.v
+++ algorithm_design/Perm.v
@@ -19,14 +19,6 @@ Fixpoint permute {X : Type} (swaps : list nat) (l : list X) : list X :=
                    end
   end.
 
-Fixpoint equivalent_insert (s : list nat) (n : nat) :=
-  match s with
-  | [] => n
-  | sh :: st => let previous := equivalent_insert st n in
-                let increment := if leb sh previous then 1 else 0 in
-                increment + previous
-  end.
-
 Search (list ?a -> list ?a -> list ?a).
 
 Theorem permute_split : forall X n s (l : list X),
@@ -54,6 +46,22 @@ Theorem firstn_app : forall X (n : nat) (a b : list X),
                b)).
 Proof.
 Admitted.
+
+Theorem firstn_append_cons : forall X (x : X) (a b : list X),
+  firstn (S (length a)) (a ++ x :: b) = a ++ [x].
+Proof.
+  intros. Search (sub (S _) _).
+  rewrite firstn_app. 
+  - rewrite -> (Nat.sub_succ_l _ _ (le_n (length a))).
+    rewrite -> Nat.sub_diag. simpl. reflexivity.
+  - apply le_S. apply le_n.
+Qed.
+
+(* firstn_all *)
+
+Search (firstn (length ?a) ?a).
+
+Check firstn_length_le.
 (* firstn_length_le *)
 
 Search firstn.
@@ -74,8 +82,73 @@ Proof.
       + fold firstn in IHl. admit.
 Admitted.
 
+Definition insert_to_permute (f : nat -> list nat -> list nat) := 
+ forall (X : Type) (i : nat) (s : list nat) (h : X) (t : list X),
+   insert i h (permute s t) = permute (f i s) (h :: t).
 
+Theorem permute_cons_O_right : forall X i s0 s1 s' h (t : list X),
+  permute s0 (permute s1 t) = permute s' t ->
+  permute (i :: s0) (permute (O :: s1) (h :: t)) =
+  permute (i :: s') (h :: t).
+Proof.
+  intros. simpl. rewrite H. reflexivity.
+Qed.
 
+Fixpoint equivalent_insert (s : list nat) (n : nat) :=
+  match s with
+  | [] => n
+  | sh :: st => let previous := equivalent_insert st n in
+                let increment := if leb sh previous then 1 else 0 in
+                increment + previous
+  end.
+
+Fixpoint valid_swaps (s : list nat) (n : nat) : Prop :=
+  match s with
+  | [] => True
+  | sh :: st => sh <= n /\ match n with (* TODO: better way to write this? *)
+                           | O => False
+                           | S pred => (valid_swaps st pred)
+                           end
+  end.
+
+Theorem two_swaps_le : forall X (n n' : nat) (h h' : X) (l : list X),
+  n <= n' ->
+  insert n h (insert n' h' l) =
+  (firstn n l) ++ h :: (firstn (sub n' n) (skipn n l)) ++ h' :: (skipn n' l).
+Proof.
+Admitted.
+
+Theorem two_swaps_le_S : forall X (n n' : nat) (h h' : X) (l : list X),
+  n' <= (S n) ->
+  insert n h (insert n' h' l) =
+  (firstn n' l) ++ h' :: (firstn (sub n' (S n)) (skipn n l)) ++ h :: (skipn n l).
+Proof.
+Admitted.
+
+Theorem swap_inserts : forall X (n n' : nat) (h h' : X) (l : list X),
+  n <= n' ->
+  insert n h (insert n' h' l) = insert n' h' (insert (S n) h l).
+Proof.
+  intros.
+  rewrite two_swaps_le. rewrite two_swaps_le_S.
+Admitted.
+  (* (firstn n l) ++ h :: (firstn (sub n' n) (skipn n l)) ++ h' :: (skip n' l) *)
+
+Theorem permute_insert : forall X s n (h : X) (l : list X),
+  valid_swaps s (length l) ->
+  permute s (insert n h l) = insert (equivalent_insert s n) h (permute s l).
+Proof.
+  induction s.
+  - reflexivity.
+  - intros. simpl. remember (insert n h l) as l0.
+    destruct l0. exfalso. apply (insert_not_empty X n h l). apply Heql0.
+    Search (_ (le _ _) (leb _ _)). destruct (Nat.leb_spec0 a (equivalent_insert s n)).
+    + admit.
+    + destruct l. destruct H as [_ contra]; simpl in contra. exfalso; apply contra.
+      apply Compare_dec.not_le in n0. apply Arith_prebase.gt_le_S_stt in n0. simpl.
+Admitted.
+
+(*
 permute s (permute (sh :: st) (h :: t))
 
 let pt = permute st t
@@ -117,22 +190,9 @@ permute (firstn sh s)
                      (permute sst (skipn sh pt))))
 
 
-Definition insert_to_permute (f : nat -> list nat -> list nat) := 
- forall (X : Type) (i : nat) (s : list nat) (h : X) (t : list X),
-   insert i h (permute s t) = permute (f i s) (h :: t).
 
-Theorem permute_cons_O_right : forall X i s0 s1 s' h (t : list X),
-  permute s0 (permute s1 t) = permute s' t ->
-  permute (i :: s0) (permute (O :: s1) (h :: t)) =
-  permute (i :: s') (h :: t).
-Proof.
-  intros. simpl. rewrite H. reflexivity.
-Qed.
-
-
-permute (i :: s0) (permute ((S pred) :: s1) (h :: t))
-permute (i :: s0) (insert (S pred) h (permute s1 t))
-
+permute s (insert n h l) = insert (equivalent_insert s n) h (permute s l).
+ *)
 
 Fixpoint inserts {X : Type} (x : X) (l s : list X) : Prop :=
   (s = List.cons x l) \/
