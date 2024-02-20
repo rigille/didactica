ab780cfde57f96ce8ac20b57eac46e5ecb17ef72
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2024-02-20 09:48:55 -0300

    verify first line of code

--- arithmetic/Verif_arithmetic.v
+++ arithmetic/Verif_arithmetic.v
@@ -9,17 +9,21 @@ Definition struct_number := Tstruct _number noattr.
 
 (* For some reason coq has trouble typechecking this if it's inlined *)
 Definition make_number (l : list Z) (v : val) : reptype struct_number :=
-  ((Vint (Int.repr (Zlength l))), v).
+  ((Vptrofs (Ptrofs.repr (Zlength l))), v).
+
+Definition digit_array {CS : compspecs} (sh : Share.t) (digit_list : list Z) (digits : val) := 
+  data_at sh (tarray tulong (Zlength digit_list)) (map Vptrofs (map Ptrofs.repr digit_list)) digits.
 
 Definition cnumber {CS : compspecs} (sh : Share.t) (digit_list : list Z) (p : val) :=
   EX digits : val,
+  !!(0 <= Zlength digit_list <= Int64.max_unsigned) &&
   data_at Ews struct_number (make_number digit_list digits) p *
-  data_at sh (tarray tulong (Zlength digit_list)) (map Vint (map Int.repr digit_list)) digits.
+  digit_array sh digit_list digits.
 
 Lemma cnumber_local_facts:
   forall sh digit_list p,
    cnumber sh digit_list p |--
-   !! (isptr p).
+       !! (isptr p /\ 0 <= Zlength digit_list <= Int64.max_unsigned).
 Proof.
   intros. unfold cnumber. Intros digits. entailer!.
 Qed.
@@ -47,15 +51,35 @@ Definition number_compare_spec : ident * funspec :=
   PRE [ tptr struct_number, tptr struct_number ]
     PROP (readable_share sh0; readable_share sh1)
     PARAMS (n0; n1)
-    SEP (cnumber sh0 d0 n0 * cnumber sh1 d1 n1)
+    SEP (cnumber sh0 d0 n0; cnumber sh1 d1 n1)
   POST [ tint ]
     PROP ()
     RETURN (comparison_int (compare d0 d1))
-    SEP (cnumber sh0 d0 n0 * cnumber sh1 d1 n1).
+    SEP (cnumber sh0 d0 n0; cnumber sh1 d1 n1).
 
 Definition Gprog : funspecs := [ number_compare_spec ].
 
 Lemma body_number_compare: semax_body Vprog Gprog f_number_compare number_compare_spec.
 Proof.
   start_function.
+  unfold cnumber. Intros digits0 digits1.
+  forward.
+  forward. simpl.
+  forward_if 
+(PROP ( )
+   LOCAL (
+   temp _t'1  (Vptrofs (Ptrofs.repr (Z.max (Zlength d1) (Zlength d0))));
+   temp _t'11 (Vptrofs (Ptrofs.repr (Zlength d1)));
+   temp _t'10 (Vptrofs (Ptrofs.repr (Zlength d0))); 
+   temp _left n0; temp _right n1)
+   SEP (
+   data_at Ews struct_number (make_number d0 digits0) n0;
+   digit_array sh0 d0 digits0;
+   data_at Ews struct_number (make_number d1 digits1) n1;
+   digit_array sh1 d1 digits1)).
+  - forward; forward. entailer!. rewrite Z.max_l.
+    reflexivity. lia.
+  - forward; forward. entailer!. rewrite Z.max_r.
+    reflexivity. lia.
+  - admit.
 Admitted.
