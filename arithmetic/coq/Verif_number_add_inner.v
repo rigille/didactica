c1854a0c6eb9c93674f10d89753e69f3a0687d39
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2024-07-31 09:19:59 -0300

    "probably wrong addition function" is indeed wrong

--- arithmetic/coq/Verif_number_add_inner.v
+++ arithmetic/coq/Verif_number_add_inner.v
@@ -7,5 +7,33 @@ Lemma body_number_add_inner: semax_body Vprog Gprog f_number_add_inner number_ad
 Proof.
   start_function.
   forward. forward. unfold make_number. forward_call.
-  forward. forward.
+  forward. forward. deadvars!. normalize.
+  remember
+    (Z.max (Zlength (number_digits left))
+           (Zlength (number_digits right)))
+  as max_length.
+  forward_for_simple_bound max_length
+  (EX j: Z,
+   PROP ()
+   LOCAL (
+     temp _i (Vlong (Int64.repr max_length));
+     lvar _carry tulong v_carry;
+     temp _left left_pointer;
+     temp _right right_pointer;
+     temp _target output_pointer)
+   SEP (
+     cnumber left left_pointer;
+     cnumber right right_pointer;
+     data_at Tsh tulong (Vlong (Int64.repr 0)) v_carry;
+     pre_cnumber output
+       (Zlength (add_digits (number_digits left) (number_digits right)))
+       output_pointer))%assert. {
+    unfold digit_bound in H, H2; rep_lia.
+  } {
+    unfold cnumber, make_number; entailer!.
+  } {
+    admit.
+  } {
+    admit.
+  }
 Admitted.
