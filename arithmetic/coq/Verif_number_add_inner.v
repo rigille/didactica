74675e657132e3a685fbeae04d58f6d514880449
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2024-08-01 08:27:02 -0300

    loop invariant is sufficient now

--- arithmetic/coq/Verif_number_add_inner.v
+++ arithmetic/coq/Verif_number_add_inner.v
@@ -1,5 +1,8 @@
+Require Import Coq.Program.Basics.
+
 Require Import VST.floyd.proofauto.
 Require Import VST.floyd.library.
+
 Require Import Didactica.arithmetic.
 Require Import Didactica.number.
 
@@ -7,11 +10,16 @@ Lemma body_number_add_inner: semax_body Vprog Gprog f_number_add_inner number_ad
 Proof.
   start_function.
   forward. unfold make_number. forward.
-  remember
-    (Zlength (add_digits
+  remember 
+    (add_digits
       (number_digits left)
-      (number_digits right)))
+      (number_digits right))
+  as total.
+  remember
+    (Zlength total)
   as final_length.
+  (* TODO: prove theorems so that unfolding here is not necessary *)
+  unfold pre_digit_array.
   forward_for_simple_bound
   final_length
   (EX j: Z,
@@ -26,13 +34,38 @@ Proof.
      cnumber left left_pointer;
      cnumber right right_pointer;
      data_at Tsh tulong (Vlong (Int64.repr 0)) v_carry;
-     pre_cnumber output final_length output_pointer))%assert. {
+     data_at
+       (pre_number_share output)
+       struct_number
+       (Vptrofs (Ptrofs.repr final_length), pre_number_array output)
+       output_pointer;
+      data_at
+        (pre_number_share output)
+        (tarray tulong final_length)
+        (app
+          (map
+            (Vptrofs oo Ptrofs.repr)
+            (sublist 0 j total))
+          (Zrepeat Vundef (final_length - j)))
+        (pre_number_array output)
+    ))%assert. {
        admit. (* TODO *)
   } {
-    unfold cnumber, pre_cnumber, make_number; entailer!.
-  } {
     admit.
   } {
     admit.
+  } {
+    replace (final_length - final_length) with 0 by lia.
+    unfold Zrepeat, Z.to_nat, repeat.
+    replace
+      (map (Vptrofs oo Ptrofs.repr) (sublist 0 final_length total) ++ [])
+    with
+      (map (Vptrofs oo Ptrofs.repr) total)
+    by list_solve.
+    unfold cnumber, make_number, fill_number, writable_pre_number,
+    readable_number.
+    entailer!. {
+      admit. (* TODO prove that addition repects bounds *)
+    } 
   }
 Admitted.
