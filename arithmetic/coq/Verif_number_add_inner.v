de6b95b242dabe37e9257b869730ce6154d751e7
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2024-08-06 07:35:57 -0300

    this must be the correct invariant, I'm feeling it

--- arithmetic/coq/Verif_number_add_inner.v
+++ arithmetic/coq/Verif_number_add_inner.v
@@ -9,7 +9,7 @@ Require Import Didactica.number.
 Lemma body_number_add_inner: semax_body Vprog Gprog f_number_add_inner number_add_inner_full_spec.
 Proof.
   start_function.
-  forward. unfold make_number. forward.
+  forward. deadvars!. forward. unfold make_number.
   (*remember 
     (add_digits
       carry
@@ -18,39 +18,58 @@ Proof.
   as total.
   remember
     (Zlength total)
-  as final_length. *)
+  as final_length.
+  unfold pre_digit_array, make_number. *)
   (* TODO: prove theorems so that unfolding here is not necessary *)
-  unfold pre_digit_array, make_number.
+  remember (pre_number_length output) as limit.
+  remember
+    (add_digits
+      carry
+      (number_digits left)
+      (number_digits right))
+  as total.
+  remember
+    (Zlength total)
+  as total_length.
   forward_for_simple_bound
-  (pre_number_length output)
-  (EX j: Z, EX carry_out: bool,
+  limit
+  (EX j : Z, EX carry_out : bool,
    PROP (
-   )
+     let trail_left :=
+       (sublist j total_length (number_digits left)) in
+     let trail_right :=
+       (sublist j total_length (number_digits right)) in
+     (eq
+       (add_digits
+         carry_out
+         trail_left
+         trail_right)
+       (sublist
+         j
+         total_length
+         total)))
    LOCAL (
-     temp _limit (Vlong (Int64.repr (pre_number_length output)));
-     lvar _carry tulong v_carry;
-     temp _left left_pointer;
-     temp _right right_pointer;
+     temp _limit (Vptrofs (Ptrofs.repr limit));
+     temp _carry (Vlong (Int64.repr (Z.b2z carry))); 
+     temp _left left_pointer; temp _right right_pointer;
      temp _target output_pointer)
    SEP (
-     cnumber left left_pointer;
-     cnumber right right_pointer;
-     data_at Tsh tulong (Vlong (Int64.repr (Z.b2z carry))) v_carry;
+     (cnumber left left_pointer);
+     (cnumber right right_pointer);
      data_at
        (pre_number_share output)
        struct_number
-       (Vptrofs (Ptrofs.repr (pre_number_length output)), pre_number_array output)
+       (Vptrofs (Ptrofs.repr limit), pre_number_array output)
        output_pointer;
-      data_at
+     data_at
         (pre_number_share output)
         (tarray tulong (pre_number_length output))
         (app
           (map
             (Vptrofs oo Ptrofs.repr)
             (sublist 0 j total))
-          (Zrepeat Vundef (final_length - j)))
-        (pre_number_array output)
-    ))%assert. {
+          (Zrepeat Vundef (total_length - j)))
+        (pre_number_array output)))%assert. {
        unfold digit_bound in H; rep_lia.
   } {
     replace 
