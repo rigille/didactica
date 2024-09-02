ed6cd60cd1da11aed0bd5664509685ac6d9555d9
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2024-09-02 06:46:59 -0300

    group all projects in the same folder

--- scheme/euler/src/main.scm
+++ scheme/euler/src/main.scm
@@ -21,9 +21,7 @@
     (letrec ((range-multiple (lambda (n)
                                (if (= n 1)
                                  1
-                                 (let ((previous (range-multip
-le (- n 1))))
+                                 (let ((previous (range-multiple (- n 1))))
                                    (/ (* n previous)
-                                      (greatest-common-divisor
- n previous)))))))
+                                      (greatest-common-divisor n previous)))))))
       (range-multiple 20)))
